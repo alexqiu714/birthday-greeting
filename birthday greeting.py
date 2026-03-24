1f27af2cd0e24c15a57cdd448e9e34729f860b55
alexqiu714 <haode.qiu@isbasel.ch>
2026-03-24 17:45:28 +0100

    Add files via upload

---
+++ birthday greeting.py
@@ -0,0 +1,21 @@
+import pygame
+pygame.init()
+
+WIDTH = 500
+HEIGHT = 500
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+screen.fill("black")
+
+cake = pygame.image.load("Lesson 3 - Brithday Greeting/images/cake.jpg") 
+cupcake = pygame.image.load("Lesson 3 - Brithday Greeting/images/cupcake.jpg")
+
+cake1 = pygame.transform.scale(cake, (500,500))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.blit(cupcake, (0,0))
+    pygame.display.update()
